eedaf4b6db89c9004d2542d6dac18c487c0f1645
mahajrod <mahajrod@gmail.com>
2023-07-26 10:18:29 +0200

    removed old dependencies on venn and scipy

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_list_of_files(list_of_dirs_and_files, expression=None, recursive=True):
 dependencies = ['numpy', 'pandas', 'matplotlib']
 
 setup(name='ChromoDoter',
-      version='0.3',
+      version='0.4',
       packages=find_packages(),
       author='Sergei F. Kliver',
       author_email='mahajrod@gmail.com',
